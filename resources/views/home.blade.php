f9ffe3ef311a0f5ee5af27acb15afd5ed6348a0d
k-srtm <no.08pm@gmail.com>
2022-11-14 19:06:01 +0900

    change home comment

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,11 +3,11 @@
 @section('title', 'Dashboard')
 
 @section('content_header')
-    <h1>Dashboard</h1>
+    <h1>商品管理システム</h1>
 @stop
 
 @section('content')
-    <p>Welcome to this beautiful admin panel.</p>
+    <p>本の管理ができるシステムです。</p>
 @stop
 
 @section('css')
